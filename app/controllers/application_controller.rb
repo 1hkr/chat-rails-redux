803f4d3412564f0424a519997f449ceb0a9c2aaa
Alexandre Gorius <ihkr@orange.fr>
2018-06-07 18:19:13 +0200

    Working version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, :set_user
-
-  private
-
-  def set_user
-    current_user ? cookies[:username] = current_user.author : cookies[:username] = 'guest'
-  end
+  before_action :authenticate_user!
 
 end
